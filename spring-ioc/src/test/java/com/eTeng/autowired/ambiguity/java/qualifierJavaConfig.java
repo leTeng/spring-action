dd8d32d3640dd1583d968ccb7c746707353f7a25
lyt <593658408@qq.com>
2018-10-30 19:14:21 +0800

    使用限定符来解决自动装配的歧义性

---
+++ spring-ioc/src/test/java/com/eTeng/autowired/ambiguity/java/qualifierJavaConfig.java
@@ -0,0 +1,13 @@
+package com.eTeng.autowired.ambiguity.java;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @FileName qualifierJavaConfig.java
+ * @Author 梁怡腾
+ * @Date 2018/10/30 16:59
+ * @Description 使用自定义的限定符解决自动装配歧义
+ */
+@Configuration
+public class qualifierJavaConfig{
+}
