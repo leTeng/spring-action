742348ca1f3c56d61fa6f6a0b7d21368c206c96e
lyt <593658408@qq.com>
2018-11-01 18:51:18 +0800

    添加Perform接口作为被通知bean

---
+++ spring-aop/src/main/java/com/eTeng/point/impl/MagicPerform.java
@@ -0,0 +1,16 @@
+package com.eTeng.point.impl;
+
+import com.eTeng.point.interfaces.Perform;
+
+/**
+ * @FileName MagicPerform.java
+ * @Author 梁怡腾
+ * @Date 2018/11/1 16:07
+ * @Description 魔术表演
+ */
+public class MagicPerform implements Perform{
+
+    public void processPerformance(){
+
+    }
+}
---
+++ spring-aop/src/main/java/com/eTeng/point/interfaces/Perform.java
@@ -0,0 +1,15 @@
+package com.eTeng.point.interfaces;
+
+/**
+ * @FileName Perform.java
+ * @Author 梁怡腾
+ * @Date 2018/11/1 16:07
+ * @Description
+ */
+public interface Perform{
+
+    /**
+     * 正在表演
+     */
+    void processPerformance();
+}
