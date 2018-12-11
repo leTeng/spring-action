9580172d10a721017dc95af544262a2ebf8164a4
lyt <593658408@qq.com>
2018-12-11 19:02:20 +0800

    添加单元测试

--- spring-aop/src/test/java/com/eTeng/java/AspectJavaConfigTest.java
+++ spring-aop/src/test/java/com/eTeng/java/AspectJavaConfigTest.java
@@ -1,7 +1,9 @@
 package com.eTeng.java;
 
 
+import com.eTeng.advice.WatchPerformCounterAdvice;
 import com.eTeng.point.interfaces.Perform;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +17,30 @@ public class AspectJavaConfigTest{
     @Autowired
     Perform perform;
 
+    @Autowired
+    WatchPerformCounterAdvice wcct;
     @Test
-    public void testPerformAop(){
+    public void testRandomPerform(){
         perform.processShow();
+    }
+
+    @Test
+    public void testSpecifyItem(){
+        perform.processShow(0);
+        perform.processShow(1);
+        perform.processShow(1);
+        perform.processShow(2);
+        perform.processShow(2);
+        perform.processShow(3);
+
+        Assert.assertEquals(1,(int)wcct.getCounter(0));
+        Assert.assertEquals(1,(int)wcct.getCounter(1));
+        Assert.assertEquals(2,(int)wcct.getCounter(2));
+        Assert.assertEquals(1,(int)wcct.getCounter(3));
+    }
+
+    @Test
+    public void testPlayAd(){
+        perform.palyAd();
     }
 }
