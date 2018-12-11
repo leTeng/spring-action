847e0539410f10c626fc349f671b2e5b2ce6d9ab
lyt <593658408@qq.com>
2018-12-11 18:59:45 +0800

    定义切面,并声明一个切点和在通知方法访问被通知方法的参数

---
+++ spring-aop/src/main/java/com/eTeng/advice/WatchPerformCounterAdvice.java
@@ -0,0 +1,42 @@
+package com.eTeng.advice;
+
+import com.eTeng.point.impl.Television;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @FileName WatchPerformAdvice.java
+ * @Author eTeng
+ * @Date 2018/12/10 17:09
+ * @Description 记录观看各个频道观众人数
+ */
+@Aspect
+public class WatchPerformCounterAdvice{
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(Television.class);
+
+    private Map<Integer,Integer> counter = new HashMap<Integer,Integer>();
+
+    //选择参数为int类型的连接点,并且将参数传递给通知方法
+    @Pointcut("execution(* com.eTeng.point.interfaces.Perform.processShow(int)) " +
+            "&& args(itemNum)")
+    public void showWithItem(int itemNum){}
+
+
+    @Before("showWithItem(itemNum)")
+    public void recordCounter(int itemNum){
+        LOGGER.info("record itemNum:" + itemNum);
+        Integer count = getCounter(itemNum);
+        counter.put(itemNum,count+1);
+    }
+
+    public Integer getCounter(int itemNum){
+        return counter.containsKey(itemNum) ? counter.get(itemNum) : 0;
+    }
+}
