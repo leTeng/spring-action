6220446e4a5f85b6577d2f223457e073f90118f4
lyt <593658408@qq.com>
2018-12-11 19:00:59 +0800

    定义为目标对象新引入的接口和默认实现

---
+++ spring-aop/src/main/java/com/eTeng/pojo/DeFaultPlayAd.java
@@ -0,0 +1,17 @@
+package com.eTeng.pojo;
+
+import com.eTeng.pojo.interfaces.PlayAd;
+
+/**
+ * @FileName DeFaultPlayAd.java
+ * @Author 梁怡腾
+ * @Date 2018/12/11 18:31
+ * @Description
+ */
+public class DeFaultPlayAd implements PlayAd{
+
+    public void playAd(){
+        System.out.println("proceed play ad");
+    }
+
+}
---
+++ spring-aop/src/main/java/com/eTeng/pojo/interfaces/PlayAd.java
@@ -0,0 +1,12 @@
+package com.eTeng.pojo.interfaces;
+
+/**
+ * @FileName AdAdvice.java
+ * @Author 梁怡腾
+ * @Date 2018/12/11 18:21
+ * @Description 播放广告
+ */
+public interface PlayAd{
+
+    void playAd();
+}
