c227d989fac68465dab3c6569168e9cba438d210
lyt <593658408@qq.com>
2018-12-11 18:58:33 +0800

    定义切面,并声明一个切点和4个基本的通知方式

--- spring-aop/src/main/java/com/eTeng/advice/PerformAdvice.java
+++ spring-aop/src/main/java/com/eTeng/advice/PerformAdvice.java
@@ -1,50 +1,57 @@
 package com.eTeng.advice;
 
-import com.eTeng.point.impl.MagicPerform;
+import com.eTeng.advice.interfaces.Advice;
+import com.eTeng.point.impl.Television;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * @FileName PerformAdvice.java
- * @Author 梁怡腾
+ * @Author eTeng
  * @Date 2018/11/2 8:59
  * @Description 表演通知类(切面)
  */
 
-@Aspect //定义为一个切面
-public class PerformAdvice{
+//@Aspect //定义为一个切面
+public class PerformAdvice implements Advice{
 
     private static final Logger LOGGER = LoggerFactory
-            .getLogger(MagicPerform.class);
+            .getLogger(Television.class);
 
     //定义一个公共切点
-    @Pointcut("execution(* com.eTeng.point.impl.MagicPerform.processShow(..))")
+    @Pointcut("execution(* com.eTeng.point.interfaces.Perform.processShow())")
     public void performShow(){
 
     }
 
-    //观众入场(调用之前)
+    //连接电源(调用之前)
     @Before("performShow()")
-    public void admission(){
-        LOGGER.info("audience admission success");
+    public void noElectricity(){
+        LOGGER.info("turn on electricity success");
     }
 
-    //鼓掌(返回之后)
+    //连接电源(调用之前)
+    @Before("performShow()")
+    public void openTv(){
+        LOGGER.info("openTv success");
+    }
+
+    //关闭(返回之后)
     @AfterReturning("performShow()")
-    public void applause(){
-        LOGGER.info("performance was wonderful");
+    public void closeTv(){
+        LOGGER.info("close success");
     }
 
-    //退票(出异常)
+    //修复(出异常)
     @AfterThrowing("performShow()")
-    public void refundTicket(){
-        LOGGER.info("performance was terrible");
+    public void fix(){
+        LOGGER.info("proceed fix");
     }
 
-    //观众退场(返回之后或出异常)
+    //断开电源(返回之后或出异常)
     @After("performShow()")
-    public void retreat(){
-        LOGGER.info("audience retreat success");
+    public void offElectricity(){
+        LOGGER.info("deenergization success");
     }
 }
