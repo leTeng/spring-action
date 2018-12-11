de41c17a0ea81a512da21532fcb3ff278a0614ff
lyt <593658408@qq.com>
2018-12-11 18:59:02 +0800

    定义切面,并声明一个切点和环绕通知方式

---
+++ spring-aop/src/main/java/com/eTeng/advice/PerformAdvice2.java
@@ -0,0 +1,43 @@
+package com.eTeng.advice;
+
+import com.eTeng.advice.interfaces.Advice;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @FileName PerformAdvice2.java
+ * @Author eTeng
+ * @Date 2018/12/10 15:36
+ * @Description 环绕通知
+ */
+
+@Aspect //声明切面
+public class PerformAdvice2{
+
+    @Autowired
+    private Advice performAdvice;
+
+    @Pointcut("execution(* com.eTeng.point.interfaces.Perform.processShow())")
+    private void processShow(){}
+
+    /**
+     * 声明环绕通知
+     * @param jp
+     */
+    @Around("processShow()")
+    public void watchPerform(ProceedingJoinPoint jp){
+        try{
+            performAdvice.noElectricity();
+            performAdvice.openTv();
+            jp.proceed(); //调用被通知方法
+//            performAdvice.closeTv();
+            performAdvice.offElectricity();
+        }catch(Throwable ex){
+            performAdvice.fix();
+        }
+
+    }
+}
