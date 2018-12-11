00765cdea73779e200318f870a2d657f0bbba763
lyt <593658408@qq.com>
2018-12-11 18:56:13 +0800

    装配切面Bean的JavaConfig

--- spring-aop/src/main/java/com/eTeng/java/AspectJavaConfig.java
+++ spring-aop/src/main/java/com/eTeng/java/AspectJavaConfig.java
@@ -1,11 +1,17 @@
 package com.eTeng.java;
 
 import com.eTeng.advice.PerformAdvice;
-import com.eTeng.point.impl.MagicPerform;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import com.eTeng.advice.PerformAdvice2;
+import com.eTeng.advice.WatchPerformCounterAdvice;
+import com.eTeng.advice.interfaces.Advice;
+import com.eTeng.introduce.PlayAdIntroduce;
+import com.eTeng.point.impl.Television;
+import com.eTeng.point.interfaces.Perform;
+import com.eTeng.pojo.DeFaultPlayAd;
+import org.springframework.context.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @FileName AspectJavaConfig.java
@@ -16,11 +22,45 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy //设置aop自动代理
-@ComponentScan(basePackageClasses = MagicPerform.class)
+@ComponentScan(basePackageClasses = Television.class)
 public class AspectJavaConfig{
 
     @Bean
-    public PerformAdvice performAdvice(){
+    @Primary
+    public Advice performAdvice(){
         return new PerformAdvice();
     }
+
+    @Bean
+    public PerformAdvice2 performAdvice2(){
+        return new PerformAdvice2();
+    }
+
+    @Bean
+    public WatchPerformCounterAdvice watchPerformCounterAdvice(){
+        return new WatchPerformCounterAdvice();
+    }
+
+    @Bean
+    public PlayAdIntroduce playAdIntroduce(){
+        return new PlayAdIntroduce();
+    }
+
+    @Bean
+    public DeFaultPlayAd deFaultPlayAd(){
+        return new DeFaultPlayAd();
+    }
+
+    @Bean
+    public Perform perform(){
+        List<String> items = new ArrayList<String>();
+        items.add("Magic");
+        items.add("Sing");
+        items.add("Dance");
+        items.add("Piano");
+        Television tv = new Television();
+        tv.setItems(items);
+        return tv;
+    }
+
 }
