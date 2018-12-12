c5beb65a2314e404d57c3e5d11f49fbb7ec1086b
lyt <593658408@qq.com>
2018-12-12 16:49:39 +0800

    修改测试方案

--- spring-aop/src/test/java/com/eTeng/java/AspectJavaConfigTest.java
+++ spring-aop/src/test/java/com/eTeng/java/AspectJavaConfigTest.java
@@ -1,6 +1,5 @@
 package com.eTeng.java;
 
-
 import com.eTeng.advice.WatchPerformCounterAdvice;
 import com.eTeng.point.interfaces.Perform;
 import org.junit.Assert;
@@ -19,11 +18,26 @@ public class AspectJavaConfigTest{
 
     @Autowired
     WatchPerformCounterAdvice wcct;
+
+    /*
+     * 测试基本通知
+     */
     @Test
     public void testRandomPerform(){
         perform.processShow();
     }
 
+    /*
+     * 测试环绕通知
+     */
+    @Test
+    public void testRandomPerform2(){
+        perform.processShow();
+    }
+
+    /*
+     * 测试通知访被通知方法的参数
+     */
     @Test
     public void testSpecifyItem(){
         perform.processShow(0);
@@ -31,16 +45,16 @@ public class AspectJavaConfigTest{
         perform.processShow(1);
         perform.processShow(2);
         perform.processShow(2);
-        perform.processShow(3);
+        perform.processShow(2);
 
         Assert.assertEquals(1,(int)wcct.getCounter(0));
-        Assert.assertEquals(1,(int)wcct.getCounter(1));
-        Assert.assertEquals(2,(int)wcct.getCounter(2));
-        Assert.assertEquals(1,(int)wcct.getCounter(3));
+        Assert.assertEquals(2,(int)wcct.getCounter(1));
+        Assert.assertEquals(3,(int)wcct.getCounter(2));
+        Assert.assertEquals(0,(int)wcct.getCounter(3));
     }
 
     @Test
     public void testPlayAd(){
-        perform.palyAd();
+//        perform.palyAd();
     }
 }
