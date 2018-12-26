fd882ef9e055b86737358a9c59459a64b6c61b92
lyt <593658408@qq.com>
2018-12-26 15:31:49 +0800

    单元测试

---
+++ spring-security/src/test/java/com/eTeng/storage/jdbc/config/ContextJavaConfigTest.java
@@ -0,0 +1,26 @@
+package com.eTeng.storage.jdbc.config;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+//
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ContextJavaConfig.class)
+public class ContextJavaConfigTest{
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void dataSource() throws Exception{
+        Connection connection = dataSource.getConnection();
+        Assert.assertNotNull(connection);
+    }
+}
---
+++ spring-security/src/test/java/com/eTeng/storage/jdbc/config/JdbcSecurityConfigTest.java
@@ -0,0 +1,12 @@
+package com.eTeng.storage.jdbc.config;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JdbcSecurityConfigTest{
+
+    @Test
+    public void configure(){
+    }
+}
